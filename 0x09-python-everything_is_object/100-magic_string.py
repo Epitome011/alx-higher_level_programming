1bcc35e89574e13272c68f71134d1bdd205f4a7d
Darlington Obioha <epitome011@yahoo.com>
2023-04-06 04:40:08 -0700

    everything object

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
-def magic_string(1=[]):
-    1 += ["BestSchool"]
-    return ", ".join(1)
+def magic_string():
+    magic_string.x = getattr(magic_string, 'x', 0) + 1
+    return ("BestSchool, " * (magic_string.x - 1) + "BestSchool")
