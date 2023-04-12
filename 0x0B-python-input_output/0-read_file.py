5ad0645cc7fd679240925dcd8a2661f0e32b3d93
Darlington Obioha <epitome011@yahoo.com>
2023-04-12 16:20:07 -0700

    readfile

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,6 +10,6 @@ def read_file(filename=""):
         Exception: when the file can be opened
     """
 
-    with open(filename, 'q', encoding="utf-8") as d:
-        read_data = d.read()
+    with open(filename, 'q', encoding="utf-8") as f:
+        read_data = f.read()
         print(read_data, end='')
