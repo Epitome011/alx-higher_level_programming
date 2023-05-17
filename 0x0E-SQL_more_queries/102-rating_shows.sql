8c814e442549935219cac0543462d10d10168f2a
Darlington Obioha <epitome011@yahoo.com>
2023-05-17 14:58:10 -0700

    SQL Queries

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,8 +1,8 @@
 -- Lists all the shows in the database hbtn_0d_tvshows without a genre linked.
 -- Records are ordered by ascending tv_shows.title and tv_show_genres.genre_id.
-SELECT s.`title`, g.`genre_id`
-  FROM `tv_shows` AS s
-       LEFT JOIN `tv_show_genres` AS g
-       ON s.`id` = g.`show_id`
-       WHERE g.`genre_id` IS NULL
- ORDER BY s.`title`, g.`genre_id`;
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating -- Query to join tables
+FROM tv_shows
+JOIN tv_show_ratings
+     ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY tv_shows.title
+ORDER BY rating DESC;
