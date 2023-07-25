cdbdfeeef6f430a7570fa37de3f0e8da8e79fb30
Darlington Obioha <epitome011@yahoo.com>
2023-07-25 16:52:44 +0100

    updated

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,18 +1,23 @@
 #!/usr/bin/node
 const request = require('request');
 
-request(process.argv[2], function (err, response, body) {
-  if (err == null) {
-    const resp = {};
-    const json = JSON.parse(body);
-    for (let i = 0; i < json.length; i++) {
-      if (json[i].completed === true) {
-        if (resp[json[i].userId] === undefined) {
-          resp[json[i].userId] = 0;
-        }
-        resp[json[i].userId]++;
+const url = process.argv[2];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const tasks = JSON.parse(body);
+
+    const completedTasksByUser = {};
+
+    tasks.forEach((task) => {
+      if (task.completed) {
+        const userId = task.userId;
+        completedTasksByUser[userId] = completedTasksByUser[userId] ? completedTasksByUser[userId] + 1 : 1;
       }
-    }
-    console.log(resp);
+    });
+
+    console.log(completedTasksByUser);
   }
 });
